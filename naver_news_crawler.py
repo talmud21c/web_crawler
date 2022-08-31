55767f754942c04350a72c513c616983115050f6
JS-Park <85037782+talmud21c@users.noreply.github.com>
2022-08-31 18:13:12 +0900

    naver news ranking

---
+++ naver_news_crawler.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+# naver 뉴스 랭킹 url
+url = 'https://news.naver.com/main/ranking/popularDay.naver?mid=etc&sid1=111'
+
+# 크롤링을 차단을 피하기 위해 헤더 정보 삽입
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
+
+# 페이지 요청
+res = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(res.text, 'html.parser')
+
+ranking_boxes = soup.find_all('div', 'rankingnews_box')
+
+ranking_news_titles = []
+
+for ranking_box in ranking_boxes:
+    article_list = ranking_box.find('ul', 'rankingnews_list').find_all('li')
+    for arti in article_list:
+        content = arti.find('div', 'list_content')
+        if content:
+            ranking_news_titles.append(content.find('a').text.strip())
+
+for title in ranking_news_titles:
+    print(title)
+
+# tag.media_end_head_headline
