6f1070c45ea625e3905f68c80cc586ed21c3225f
JS-Park <85037782+talmud21c@users.noreply.github.com>
2022-10-31 23:53:26 +0900

    Update rurliweb_crawler.py

--- rurliweb_crawler.py
+++ rurliweb_crawler.py
@@ -6,27 +6,21 @@ def main(url):
 
     # 본문 제목
     article_title = soup.find('span', attrs={'class': 'subject_inner_text'}).get_text().strip()
-    print(article_title)
 
     # 작성자
     nickname = soup.find('strong', attrs={'class': 'nick'}).get_text().strip()
-    print(nickname)
 
     # 작성일
     created_at = soup.find('span', attrs={'class': 'regdate'}).get_text().strip()
-    print(created_at)
 
     # 작성자 IP - ip span 태그에 css class가 없으므로 css 선택자 사용
     ip_address = soup.select_one('div.col.user_info_wrapper > div > p:nth-child(7) > span').get_text().strip()
-    print(ip_address)
 
     # 본문 내용
     article = soup.find('article', attrs={'class': None}).get_text().strip()
-    print(article)
 
     # 추천 수
     recommend_cnt = soup.find('span', attrs={'class': 'like'}).get_text().strip()
-    print(recommend_cnt)
 
     # 조회 수
     # soup.find('div', attrs={'class': 'user_info'})
@@ -39,11 +33,24 @@ def main(url):
     # 댓글 수 - [, ] 괄호 제거
     comment_cnt_bucket = soup.find('strong', attrs={'class': 'reply_count'}).get_text().strip()
     comment_cnt = str(comment_cnt_bucket)[1:-1]
-    print(comment_cnt)
 
-    print(article_title)
+    print('제목: ' + article_title + '\n',
+          '작성자: ' + nickname + '\n',
+          '작성일: ' + created_at + '\n',
+          '작성자 IP: ' + ip_address + '\n',
+          '본문 내용: ' + article + '\n',
+          '추천 수: ' + recommend_cnt + '\n',
+          '조회 수: ' + view_cnt + '\n',
+          '댓글 수: ' + comment_cnt)
+
+    # 댓글
+    comments = []
+    comment_all = soup.find_all('span', attrs={'class': 'text'})
+    for comment in comment_all:
+        comments.append(comment.get_text().strip())
+    print(comments)
 
 
 if __name__ == '__main__':
-    webpage_url = 'https://bbs.ruliweb.com/best/board/300143/read/59056711'
-    main(webpage_url)
+    url_input = input('루리웹 게시글 url을 입력해주세요: ')
+    main(url_input)
