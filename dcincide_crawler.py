bdef4d1b450ef673a25d987539535b702f37e869
JS-Park <85037782+talmud21c@users.noreply.github.com>
2022-11-02 16:00:04 +0900

    Update dcincide_crawler.py

--- dcincide_crawler.py
+++ dcincide_crawler.py
@@ -17,33 +17,39 @@ def main(url):
     # 조회 수
     view_cnt = soup.find('span', attrs={'class': 'gall_count'}).get_text().strip()
 
+    # 추천 수
+    like_cnt = soup.find('p', attrs={'class': 'up_num'}).get_text().strip()
+
     # 댓글 수
     comment_cnt = soup.find('span', attrs={'class': 'gall_comment'}).get_text().strip()
     cmt_cnt = comment_cnt.replace('댓글', '')
-    print(int(cmt_cnt))
 
     # 본문 내용
-    content_all = soup.find('div', attrs={'class': 'writing_view_box'})
-    content = content_all.p.get_text().strip()
+    content = soup.find('div', attrs={'class': 'write_div'}).get_text().strip()
 
     # 등록일
     created_at = soup.find('span', attrs={'class': 'gall_date'}).get_text().strip()
 
-    article_data = (article_title, nickname, ip_address, view_cnt, comment_cnt, content, created_at)
-    print(article_data)
+    print(
+        '제목: {}\n'.format(article_title),
+        '작성자: {}\n'.format(nickname),
+        '작성자 ip: {}\n'.format(ip_address),
+        '작성일: {}\n'.format(created_at),
+        '조회수: {}\n'.format(view_cnt),
+        '추천수: {}\n'.format(like_cnt),
+        '본문 내용: {}\n'.format(content),
+        '댓글수: {}\n'.format(int(cmt_cnt))
+    )
 
     # ---------------------------댓글 출력 테스트----------------------------------
     # 일반적인 페이지 요청으로 댓글을 로드하지 못 함.
     # 댓글 요청은 post로 요청
     comments = []
     comment_elements = soup.find_all('li', attrs={'class': 'ub-content'})
-    print(comment_elements)
 
     nickname_all = soup.find_all('span', 'nickname')
-    print(nickname_all)
 
     comment = soup.find_all('p', attrs={'class': 'ub-word'})
-    print(comment)
 
     for element in comment_elements:
         nickname = soup.find('em', attrs={'title': '닉네임'})
@@ -59,8 +65,6 @@ def main(url):
         }
 
         comments.append(comment)
-
-    print(comments)
 
 
 if __name__ == '__main__':
